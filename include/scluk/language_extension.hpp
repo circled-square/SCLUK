a04e65bb1e980597178e878acdc8d0f2c3c2468a
ordinatrice <circled-square@github.com>
2021-08-06 12:42:01 +0200

    renamed modern_print.hpp to format.hpp and added string.hpp to language_extension.hpp

--- include/scluk/language_extension.hpp
+++ include/scluk/language_extension.hpp
@@ -1,14 +1,16 @@
 #ifndef SCLUK_LANGUAGE_EXTENSION_HPP
 #define SCLUK_LANGUAGE_EXTENSION_HPP
 
-#include "modern_print.hpp"
+#include "format.hpp"
 #include "range.hpp"
 #include "aliases.hpp"
+#include "string.hpp"
 
 namespace scluk::language_extension {
 	using namespace scluk::type_aliases;
 	using namespace scluk::ranges;
 	using namespace scluk::fmt;
+	using namespace scluk::str;
 }
 
 #endif //SCLUK_LANGUAGE_EXTENSION_HPP
