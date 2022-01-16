879fd3db9ece9519db1fc23f5fb9f95e5558eeee
circled-square <circled-square@github.com>
2022-01-16 15:22:03 +0100

    added string.cpp so the function templates in string.hpp can be specialized and their specializations compiled into an object file

---
+++ include/scluk/definition/string.cpp
@@ -0,0 +1,17 @@
+#include "../string.hpp"
+#include "../aliases.hpp"
+namespace scluk::str {
+    #define EXPLICIT_TEMPLATE_INSTANTIATION(T) \
+        template std::basic_string<T> repeat(T c, std::size_t n); \
+        template std::basic_string<T> repeat(const std::basic_string<T>& s, std::size_t n); \
+        template std::basic_string<T> operator*(const std::basic_string<T>& s, std::size_t n); \
+        template int levenshtein_dist(std::basic_string_view<T> a, std::basic_string_view<T> b);
+
+    EXPLICIT_TEMPLATE_INSTANTIATION(char)
+    EXPLICIT_TEMPLATE_INSTANTIATION(uchar)
+    EXPLICIT_TEMPLATE_INSTANTIATION(schar)
+    EXPLICIT_TEMPLATE_INSTANTIATION(char8_t)
+    EXPLICIT_TEMPLATE_INSTANTIATION(char16_t)
+    EXPLICIT_TEMPLATE_INSTANTIATION(char32_t)
+    EXPLICIT_TEMPLATE_INSTANTIATION(wchar_t)
+}
