c70bb2d8b841834db1e82eb11271749592e8d10f
circled-square <circled-square@github.com>
2022-02-13 22:35:50 +0100

    worked on result for long enough i realised i should have just learnt rust instead

--- include/scluk/result.hpp
+++ include/scluk/result.hpp
@@ -4,81 +4,127 @@
 #include <variant>
 #include <optional>
 #include <string>
-#include <stdexcept>
+#include <memory>
+#include "exception.hpp"
+#include "metaprogramming.hpp"
 
 namespace scluk {
-    template<class ret_t, class warning_t = std::string>
-    class result {
-    public:
-        using exception_ptr_t = std::unique_ptr<std::exception>;
-        using variant_t = std::variant<ret_t, exception_ptr_t>;
-        variant_t res_or_except;
-        std::optional<warning_t> warning;
+    template<std::default_initializable ret_t, class warning_t = std::string> requires(!std::derived_from<warning_t, std::exception>)
+    struct successful_result {
+        ret_t r; 
+        std::optional<warning_t> w;
 
-        result(result&& o) : res_or_except(std::move(o.res_or_except)), warning(std::move(o.warning)) {}
-        result(ret_t res, std::optional<warning_t> warning = {}) : res_or_except(res), warning(warning) {}
-        result(const std::exception& e) : res_or_except(std::make_unique<std::exception>(std::move(e))) {}
-        template<std::derived_from<std::exception> except_t>
-        result(const except_t& e) : res_or_except(std::make_unique<std::exception>(std::move(e))) {}
-        result(exception_ptr_t&& e) : res_or_except(std::move(e)) {}
+        successful_result(ret_t r, std::optional<warning_t> w = {}) : r(r), w(w) {}
 
-        result set(ret_t v) { res_or_except = v; return *this; }
-        result warn(warning_t w) { warning = w; }
-        void fail(std::exception e) { res_or_except = e; }
+        ret_t unwrap() { return r; }
 
-        //throw exceptions, ignore warnings
-        ret_t unwrap() {
-            return unwrap(scluk::throw_exception);
+        template<concepts::fn<void(warning_t)> warning_cb_t>
+        ret_t unwrap(warning_cb_t warning_cb) {
+            if(w) warning_cb(*w);
+            return r;
         }
+
+        ret_t unwrap(std::optional<warning_t>& w) {
+            w = this->w;
+            return r;
+        }
+
+        std::tuple<ret_t, std::optional<warning_t>> to_tuple(){ return {r,w}; }
+    };
+
+    template<std::default_initializable ret_t, class warning_t = std::string> requires(!std::derived_from<warning_t, std::exception>)
+    class result {
+        using exception_ptr_t = std::unique_ptr<std::exception>;
+        using success = successful_result<ret_t, warning_t>;
+        using variant_t = std::variant<exception_ptr_t, success>;
+        variant_t m_contents;
+
+        exception_ptr_t& get_except_ptr(){ return std::get<exception_ptr_t>(m_contents); }
+        success& get_success_struct() { return std::get<success>(m_contents); }
+    public:
+        /* assumes state is 'exception' */
+        std::exception& get_except() { return *get_except_ptr(); }
+        /* assumes state is 'success' */
+        ret_t& get_result(){ return get_success_struct().r; }
+        std::optional<warning_t>& get_opt_warning(){ return get_success_struct().w; }
+        warning_t& get_warning(){ return *get_success_struct().w; }
+
+        bool has_except(){ return std::holds_alternative<exception_ptr_t>(m_contents); }
+        bool has_result(){ return std::holds_alternative<success>(m_contents); }
+        bool has_warning(){ return has_result() && std::get<success>(m_contents).w; }
+
+        /* sets the state to 'success' */
+        result() : m_contents(success{ret_t(),{}}) {}
+        result(ret_t r) : m_contents(success{std::move(r), {}}) {}
+        result(ret_t r, warning_t w) : m_contents(success{std::move(r), std::move(w)}) {}
+        result(ret_t r, std::optional<warning_t> w) : m_contents(success{std::move(r), std::move(w)}) {}
+        /* sets the state to 'exception' */
+        template<std::derived_from<std::exception> except_t>
+        result(const except_t& e) : m_contents(std::make_unique<std::exception>(std::move(e))) {}
+        result(exception_ptr_t&& e) : m_contents(std::move(e)) {}
+        /* mimics the moved object's state*/
+        result(result&& o) : m_contents(std::move(o.m_contents)) {}
+        
+        /* sets the state to 'success' */
+        result set(ret_t v, std::optional<warning_t> w) {
+            m_contents = {v, w};
+            return *this; 
+        }
+        /* assumes state is 'success' */
+        result warn(warning_t w) { get_warning() = w; return *this; }
+        /* sets the state to 'exception' */
+        void fail(std::exception e) { m_contents = std::make_unique<std::exception>(std::move(e)); }
+        void fail(std::exception_ptr e) { m_contents = std::move(e); }
+
+        /*
+         *  unwrap will return the contained value, coping with any errors or warnings the way the user specifies. 
+         *  assumes state isn't 'nothing'.
+         */
+        //throw exceptions, ignore warnings
+        ret_t unwrap() { return unwrap(throw_exception); }
 
         //throw exceptions, cb for warnings
-        template<scluk::concepts::func<void(warning_t)> warning_cb_t>
-        ret_t unwrap(warning_cb_t warning_cb) {
-            return unwrap(scluk::throw_exception, warning_cb);
-        }
+        template<concepts::fn<void(warning_t)> warning_cb_t>
+        ret_t unwrap(warning_cb_t warning_cb) { return unwrap(throw_exception, warning_cb); }
         
         //cb for exceptions, ignore warnings
-        template<scluk::concepts::func<ret_t(std::exception)> except_cb_t>
+        template<concepts::fn<ret_t(std::exception)> except_cb_t>
         ret_t unwrap(except_cb_t except_cb) {
-            if(std::holds_alternative<ret_t>(res_or_except))
-                return std::move(std::get<ret_t>(res_or_except));
-            else return except_cb(std::move(std::get<exception_ptr_t>(res_or_except)));
+            if(has_except()) return except_cb(get_except());
+            return std::move(get_result());
         }
 
         //cb for exceptions and for warnings
         template<
-            scluk::concepts::func<ret_t(std::exception)> except_cb_t, 
-            scluk::concepts::func<void(warning_t)> warning_cb_t>
+            concepts::fn<ret_t(std::exception)> except_cb_t, 
+            concepts::fn<void(warning_t)> warning_cb_t> 
         ret_t unwrap(except_cb_t except_cb, warning_cb_t warning_cb) {
-            if(std::holds_alternative<ret_t>(res_or_except)) {
-                if(warning) warning_cb(*warning);
-                return std::move(std::get<ret_t>(res_or_except));
-            } else return except_cb(std::move(std::get<exception_ptr_t>(res_or_except)));
+            if(has_except()) return except_cb(get_except());
+            if(has_warning()) warning_cb(get_warning());
+            return std::move(get_result());
         }
         
         //rethrowing cb for exception, ignore warnings
-        template<scluk::concepts::func<void(std::exception)> except_cb_t>
+        template<concepts::fn<void(std::exception)> except_cb_t>
         ret_t unwrap(except_cb_t except_cb) {
-            if(std::holds_alternative<ret_t>(res_or_except))
-                return std::get<ret_t>(res_or_except);
-            else {
-                except_cb(std::move(*std::get<exception_ptr_t>(res_or_except)));
-                throw std::runtime_error("func except_cb passed to unwrap does neither return the correct type nor throw");
+            if(has_except()) {
+                except_cb(get_except());
+                throw get_except();
             }
+            return std::move(get_result());
         }
 
         //rethrowing cb for exception, cb for warnings
         template<
-            scluk::concepts::func<void(std::exception)> except_cb_t, 
-            scluk::concepts::func<void(warning_t)> warning_cb_t>
-        ret_t unwrap(except_cb_t exception_cb, warning_cb_t warning_cb) {
-            if(std::holds_alternative<ret_t>(res_or_except)) {
-                if(warning) warning_cb(*warning);
-                return std::get<ret_t>(res_or_except);
-            } else {
-                exception_cb(std::move(*std::get<exception_ptr_t>(res_or_except)));
-                throw std::runtime_error("func except_cb passed to unwrap does neither return the correct type nor throw");
+            concepts::fn<void(std::exception)> except_cb_t, 
+            concepts::fn<void(warning_t)> warning_cb_t>
+        ret_t unwrap(except_cb_t except_cb, warning_cb_t warning_cb) {
+            if(has_except()) {
+                except_cb(get_except());
+                throw get_except();
             }
+            if(has_warning()) warning_cb(get_warning());
+            return std::move(get_result());
         }
     };
 }
