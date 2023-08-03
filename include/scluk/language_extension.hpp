d31bd79cd250fd614290a5fd8b11ef8d02d7fa83
circled-square <circled-square@github.com>
2023-08-03 15:23:08 +0200

    removed inline namespace ranges; removed range shorthand sintax start_> (it was bad)

--- include/scluk/language_extension.hpp
+++ include/scluk/language_extension.hpp
@@ -5,8 +5,4 @@
 #include "range.hpp"
 #include "aliases.hpp"
 
-namespace scluk::language_extension {
-	using namespace scluk::ranges;
-}
-
 #endif //SCLUK_LANGUAGE_EXTENSION_HPP
--- include/scluk/range.hpp
+++ include/scluk/range.hpp
@@ -8,34 +8,25 @@
 #include "aliases.hpp"
 
 namespace scluk {
-    inline namespace ranges {
-        struct range {
-            const i64 lower_bound, upper_bound;
+    struct range {
+        const i64 lower_bound, upper_bound;
 
-            range(i64 upper);
-            range(size_t upper);
-            range(i64 lower, i64 upper);
-            range(size_t lower, size_t upper);
+        range(i64 upper);
+        range(size_t upper);
+        range(i64 lower, i64 upper);
+        range(size_t lower, size_t upper);
 
-            struct iterator {
-                i64 el;
-                bool     operator!=(i64 o);
-                iterator operator++();
-                i64&     operator* ();
-            };
-            iterator begin();
-            i64 end();
+        struct iterator {
+            i64 el;
+            bool     operator!=(i64 o);
+            iterator operator++();
+            i64&     operator* ();
         };
-        
-        inline range index(const auto& c) { return range(c.size()); }
+        iterator begin();
+        i64 end();
+    };
 
-        struct range_shorthand_helper {
-            const i64 lower_bound;
-            range operator>(i64 upper_bound);
-            range_shorthand_helper operator-();
-        };
-        range_shorthand_helper operator""_(unsigned long long lower_bound);
-    }
+    inline range index(const auto& c) { return range(c.size()); }
 }
 
 #endif
