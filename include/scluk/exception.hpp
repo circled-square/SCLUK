7621bea9524dfcff4c6877a6474b4bca24dcbffc
circled-square <46634383+circled-square@users.noreply.github.com>
2024-06-20 13:32:20 +0200

    better comments

--- include/scluk/exception.hpp
+++ include/scluk/exception.hpp
@@ -5,7 +5,7 @@
 #include <format>
 
 namespace scluk {
-    //if need be, transform this into a macro to be used for any exception
+    //if need be define other functions for other exceptions or transform this into a macro to be used for any exception
     template<typename... Ts>
     std::runtime_error runtime_error(std::format_string<const Ts&...> fmt, const Ts&...args) {
         return std::runtime_error(std::format(fmt, args...));
--- include/scluk/functional.hpp
+++ include/scluk/functional.hpp
@@ -10,7 +10,7 @@ namespace scluk {
     template<typename signature_t> using fnptr_t = typename meta::signature_helper<signature_t>::fptr_type;
 
     //takes ownership of a callable object (moving it) and returns a function pointer calling it with minimum overhead.
-    //if the type used is not unique (std::function as opposed to lambdas) a unique id needs to be used for each callable with the same type
+    //if the type used is not unique to that instance a unique id needs to be used for each callable with the same type
     //uses assert to check if the id is reused with the same type
     template<typename signature_t, class lambda_t, u64 id = 0>
     static fnptr_t<signature_t> lambda_to_fnptr(lambda_t&& fn) {
