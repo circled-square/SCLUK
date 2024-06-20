eafef63e62ecf505c5a543f4ddb1ce468fa558b0
circled-square <46634383+circled-square@users.noreply.github.com>
2024-06-20 13:32:04 +0200

    improved definition of aliases for floats, now checking their size in bits is actually 32/64/128

--- include/scluk/aliases.hpp
+++ include/scluk/aliases.hpp
@@ -3,6 +3,8 @@
 
 #include <cstdint>
 #include <cstddef>
+#include <type_traits>
+#include <climits>
 
 namespace scluk {
     using i8    = std::int8_t;
@@ -13,16 +15,20 @@ namespace scluk {
     using u16   = std::uint16_t;
     using u32   = std::uint32_t;
     using u64   = std::uint64_t;
-    using f32   = float;
-    using f64   = double;
-    using f128  = long double;
-    using b8    = bool;
+
     using imax  = std::intmax_t;
     using umax  = std::uintmax_t;
     using uint  = unsigned;
     using uchar = unsigned char;
     using schar = signed char;
     using std::size_t;
+
+    using f32   = std::conditional_t<sizeof(float) * CHAR_BIT == 32, float, void>;
+    using f64   = std::conditional_t<sizeof(double) * CHAR_BIT == 64, double, void>;
+    using f128  = std::conditional_t<sizeof(long double) * CHAR_BIT == 128, long double, void>;
+    using fmax  = long double;
+
+    //define integer literal operators _u8, _u16 ... _i8, _i16 ...
     #define TMP_MACRO(T) constexpr T operator""_##T(unsigned long long n) { return T(n); }
         TMP_MACRO(u8)TMP_MACRO(u16)TMP_MACRO(u32)TMP_MACRO(u64)TMP_MACRO(umax)
         TMP_MACRO(i8)TMP_MACRO(i16)TMP_MACRO(i32)TMP_MACRO(i64)TMP_MACRO(imax)
