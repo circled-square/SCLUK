2fd8434fd39aac1403a7a76ac1c7e16e35468fbf
circled-square <46634383+circled-square@users.noreply.github.com>
2024-07-06 20:54:25 +0200

    added exception for when scluk::read_file cannot find the file

--- include/scluk/read_file.hpp
+++ include/scluk/read_file.hpp
@@ -3,15 +3,18 @@
 
 #include <fstream>
 #include <vector>
+#include <format>
 
 namespace scluk {
     inline std::string read_file(const char* fname) {
         std::ifstream fi(fname);
+        if (fi.fail())
+            throw std::runtime_error(std::format("scluk::read_file could not read file at path '{}'", fname));
         std::string contents((std::istreambuf_iterator<char>(fi)), std::istreambuf_iterator<char>());
         return contents;
     }
 	inline std::string read_file(const std::string& fname) { return read_file(fname.c_str()); }
-    
+
     inline std::vector<unsigned char> read_binary_file(const char* fname) {
         using uchar = unsigned char;
         std::basic_ifstream<uchar> fi(fname, std::ios_base::binary);
